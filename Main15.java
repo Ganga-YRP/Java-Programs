bf483c4f8e195af2bf60a90ca8e56424e4c5487c
Ganga-YRP <beedammalleswari2005@gmail.com>
2025-06-03 16:04:34 +0530

    Add files via upload

---
+++ Main15.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main15
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String a=sc.nextLine();
+	    String b=sc.nextLine();
+	    if(a.equals(b)){
+	        System.out.println(true);
+	        System.exit(0);
+	    }
+	    
+	    System.out.println(false);
+}
+}
