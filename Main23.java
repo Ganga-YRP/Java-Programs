8eeb6b7b34f0bd1694250d43e72279db580d3670
Ganga-YRP <beedammalleswari2005@gmail.com>
2025-06-03 16:07:32 +0530

    Add files via upload

---
+++ Main23.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+import java.util.*;
+public class Main23{
+    public static void main(String[] args){
+        Stack<Integer> st=new Stack<>();
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+   for(int i=0;i<s.length();i++){
+       char ch=s.charAt(i);
+       if(Character.isDigit(ch)){
+           st.push(Character.getNumericValue(ch));
+       }
+       else{
+           int a=st.pop();
+           int b=st.pop();
+           if(ch=='+')st.push(a+b);
+           if(ch=='-')st.push(a-b);
+           if(ch=='*')st.push(a*b);
+           if(ch=='/')st.push(a/b);
+       }
+   }
+   System.out.println(st.pop());
+    }
+}
