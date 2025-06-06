2c6217666d278435bbb632b823950eb8ded22c13
Ganga-YRP <beedammalleswari2005@gmail.com>
2025-06-06 15:56:02 +0530

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,79 @@
+class Node{
+    int data;
+    Node right;
+    Node left;
+    Node(int n){
+        this.data=n;
+        this.left=null;
+        this.right=null;
+    }
+}
+class tree{
+    Node root;
+    tree(){
+        this.root=null;
+    }
+    void insert(int n){
+    Node newnode=new Node(n);
+    if(root==null){
+        this.root=newnode;
+    }
+    else{
+        Node curr=this.root;
+        while(true){
+            if(n>curr.data){
+                if(curr.right==null){
+                    curr.right=newnode;
+                    break;
+                }
+                else{
+                    curr=curr.right;
+                }
+                if(curr.left==null){
+                    curr.left=newnode;
+                    break;
+                }
+                else{
+                    curr=curr.left;
+                }
+            }
+        }
+    }
+    }
+    void Postorder(Node root){
+        if(root!=null){
+            Preorder(root.left);
+            Preorder(root.right);
+            System.out.println(root.data);
+        }
+    }
+    void Inorder(Node root){
+        if(root!=null){
+            Inorder(root.left);
+            System.out.println(root.data);
+            Inorder(root.right);
+        }
+    }
+    void Preorder(Node root){
+        if(root!=null){
+            System.out.println(root.data);
+            Preorder(root.left);
+            Preorder(root.right);
+        }
+    }
+        
+    
+}
+public class Tree
+{
+	public static void main(String[] args){
+	   tree t=new tree();
+	   t.insert(1);
+	   t.insert(3);
+	   t.insert(5);
+	   t.Preorder(t.root);
+	   t.Inorder(t.root);
+	   t.Postorder(t.root);
+	}
+	
+}
