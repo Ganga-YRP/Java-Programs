77450ab385847b3eafb86831493a7a638ccbe02c
Ganga-YRP <beedammalleswari2005@gmail.com>
2025-06-06 15:58:09 +0530

    Add files via upload

---
+++ FunctionS.java
@@ -0,0 +1,12 @@
+public class FunctionS
+{
+    public static void add(){
+        int a=3;
+        int b=5;
+        int c=a+b;
+        System.out.println(c);
+    }
+	public static void main(String[] args){
+	  add();
+	}
+}
