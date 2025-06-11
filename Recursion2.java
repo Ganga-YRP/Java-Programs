82ced5f049f27b604f870d6a496d1bc3f7026a1c
Ganga-YRP <beedammalleswari2005@gmail.com>
2025-06-11 14:42:22 +0530

    Add files via upload

---
+++ Recursion2.java
@@ -0,0 +1,12 @@
+public class Recursion2
+{
+	public  void fun(int n) {
+	    if(n==11) return;
+	    System.out.println(n);
+	    fun(n+1);
+	}
+	public static void main(String[]args){
+	    Main m=new Main();
+	    m.fun(1);
+	}
+}
